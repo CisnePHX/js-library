68e777a68eff3be5ed3c5de83da920367d67a267
HDBarton <heatherdbarton@gmail.com>
2022-09-29 23:38:29 -0700

    Add library array and function

--- functions.js
+++ functions.js
@@ -1,11 +1,23 @@
 //Core library code here
 
+//Create an array to store the books
+let myLibrary = [];
+
+
+
+
 //Create a base class constructor for a book object
 function Book(title, author, pages, readYet){
     this.title = titlethis.author = author
     this.pages = pages
     this.readYet = readYet
     this.info = function() {
-        console.log(title + " by " + author + ", " + pages + ", " + readYet)
+        infoString = title + " by " + author + ", " + pages + ", " + readYet;
+        return infoString;
     }
 }
+
+//Add the book to the library
+function addBookToLibrary(Book){
+    myLibrary.push(Book);
+}   
