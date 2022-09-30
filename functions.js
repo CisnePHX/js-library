f815b31321ea46c4a1cdb2ae9ca2fa18955aefce
HDBarton <heatherdbarton@gmail.com>
2022-09-29 23:24:25 -0700

    Added base class constructor

--- functions.js
+++ functions.js
@@ -1,1 +1,11 @@
 //Core library code here
+
+//Create a base class constructor for a book object
+function Book(title, author, pages, readYet){
+    this.title = titlethis.author = author
+    this.pages = pages
+    this.readYet = readYet
+    this.info = function() {
+        console.log(title + " by " + author + ", " + pages + ", " + readYet)
+    }
+}
