b76905f64bc3b6f909a2d1df73192dc94ce0f61e
HDBarton <heatherdbarton@gmail.com>
2022-10-01 00:58:37 -0700

    Added books to array

--- functions.js
+++ functions.js
@@ -8,9 +8,9 @@ let myLibrary = [];
 
 //Create a base class constructor for a book object
 function Book(title, author, pages, readYet){
-    this.title = titlethis.author = author
-    this.pages = pages
-    this.readYet = readYet
+    this.title = titlethis.author = author;
+    this.pages = pages;
+    this.readYet = readYet;
     this.info = function() {
         infoString = title + " by " + author + ", " + pages + " pages, " + readYet;
         return infoString;
@@ -24,13 +24,23 @@ function addBookToLibrary(Book){
 
 //Add some permanent books to your library for function testing
 const timeReborn = new Book("Time Reborn", "Lee Smolin", "274", "not read yet");
+addBookToLibrary(timeReborn);
 const dirt = new Book("Dirt", "William Brian Logan", "201", "partially read");
+addBookToLibrary(dirt);
 const physicsBook = new Book("The Physics Books", "Sterling Publishing House", "506", "not read yet");
-const tradHealersCentralAustralia = new Book("Traditional Healers of Central Australia", "Various Authors", "304", "partially read);
-
+addBookToLibrary(physicsBook);
+const tradHealersCentralAustralia = new Book("Traditional Healers of Central Australia", "Various Authors", "304", "partially read");
+addBookToLibrary(tradHealersCentralAustralia);
 
 
 //Loop through the array and display each book - format it however you think would be best 
+for(let i=0; i< myLibrary.length; i++){
+    let newBookLine = document.createElement('div');
+    let bookPrintInfo = Book.info();
+}
+
+
+
 
 //Add a button that brings up a form allowing users to input the details for the new book: author, title, number of pages, whether it’s been read
 
