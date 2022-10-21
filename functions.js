076708d6fc3db7582fe3e6a5d86ebf9cd7ef9c6c
HDBarton <heatherdbarton@gmail.com>
2022-10-21 12:58:22 -0700

    Add function to add new book to library

--- functions.js
+++ functions.js
@@ -97,12 +97,20 @@ function closeForm(){
     document.getElementById("bookForm").style.display = "none";
 }
 
-//document.getElementById("open-form").onclick = function() {openAddBook()};
-
 //Add a button on each book’s display to remove the book from the library
 ////////////////  need to associate your DOM elements with the actual book objects in some way.
 //////////////    One easy solution is giving them a data-attribute that corresponds to the index of the library array.
-
+function addNewBook(){
+    const form = document.getElementById("addForm");
+    const newTitle = form.elements['title'];
+    const newAuthor = form.elements['author'];
+    const newPages = form.elements['pages'];
+    let addTitle = newTitle.value;
+    let addAuthor = newAuthor.value;
+    let addPages = newPages.value;
+    const newLibraryBook = new Book('${addTitle}', '${addAuthor}', '${addPages}', 'unread');
+    addBookToLibrary(newLibraryBook);
+}
 
 //Add a button on each book’s display to change its read status
 //////////////  To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance
