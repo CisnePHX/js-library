c2405f94c0b279809957df56efa4cb8bbd50d195
HDBarton <heatherdbarton@gmail.com>
2022-10-22 20:14:14 -0700

    Change library build to a function call

--- functions.js
+++ functions.js
@@ -119,6 +119,7 @@ function addNewBook(){
     deleteLibraryDisplay();
 
 }
+buildLibrary();
 
 //Add a button on each book’s display to change its read status
 //////////////  To facilitate this you will want to create the function that toggles a book’s read status on your Book prototype instance
